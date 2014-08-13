8330cff30184ddbf036ea0e2f5cbb96f4a3ea695
Mike <mikeh09@gmail.com>
2014-08-13 17:41:41 -0500

    Handles non-array objects and null, ignores functions. Still failing some tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,46 +5,43 @@
 var stringifyJSON = function(obj) {
   var converted = '';
   
-  if(obj === undefined) {
+  if(obj === undefined || typeof(obj) === 'function') {
     return converted;
   }
-  else if(typeof(obj) === 'string') {
+  if(typeof(obj) === 'string') {
     converted += '\"' + obj + '\"';
+  }
+  else if(obj === null) {
+	converted += obj;
   }
   else if(Array.isArray(obj)) {
     converted += '[' 
 	for(var i = 0; i < obj.length; i++) {
 	  converted += stringifyJSON(obj[i]);
 	  if(i < obj.length-1) {
-	    converted += ",";
+	    converted += ',';
 	  }
 	}
 	converted += ']';
   }
+  else if(typeof(obj) === 'object' && !Array.isArray(obj)) {
+    converted += '{';
+	var chop = false;
+	for(var key in obj) {
+	  converted += stringifyJSON(key);
+	  converted += ':';
+	  converted += stringifyJSON(obj[key]);
+	  converted += ',';
+	  chop = true;
+	}
+	if(chop) {
+	  converted = converted.substring(0, converted.length - 1);
+	}
+	converted += '}';
+  }
   else {
     converted += obj;
-  }
-  
-  
-  /*
-  if(Array.isArray(obj)){
-    converted += "[";
-    for(var i = 0; i < obj.length; i++){
-	  converted += obj[i];
-	  converted += "\n";
-	}
-	converted += "]";
-  }
-  else{
-    converted += "{";
-    for(var key in obj){
-      converted += key;
-	  converted += ": ";
-	  converted += obj[key];
-    }
-	converted += "}";
-  }
-  */ 
+  } 
  
   return converted;
 }
