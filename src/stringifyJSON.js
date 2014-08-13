69b85c91115c4eabea7a2ddca26778511bf3fab8
Mike <mikeh09@gmail.com>
2014-08-13 17:25:52 -0500

    stringifyJSON should handle undefined, strings, arrays, and regular values. Passes first 12 input tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,48 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  var converted = '';
+  
+  if(obj === undefined) {
+    return converted;
+  }
+  else if(typeof(obj) === 'string') {
+    converted += '\"' + obj + '\"';
+  }
+  else if(Array.isArray(obj)) {
+    converted += '[' 
+	for(var i = 0; i < obj.length; i++) {
+	  converted += stringifyJSON(obj[i]);
+	  if(i < obj.length-1) {
+	    converted += ",";
+	  }
+	}
+	converted += ']';
+  }
+  else {
+    converted += obj;
+  }
+  
+  
+  /*
+  if(Array.isArray(obj)){
+    converted += "[";
+    for(var i = 0; i < obj.length; i++){
+	  converted += obj[i];
+	  converted += "\n";
+	}
+	converted += "]";
+  }
+  else{
+    converted += "{";
+    for(var key in obj){
+      converted += key;
+	  converted += ": ";
+	  converted += obj[key];
+    }
+	converted += "}";
+  }
+  */ 
+ 
+  return converted;
+}
